627bbe8aa7d8cbb1f2dc21ec2b9db31479827feb
nathy981 <candido.nathaly@escola.pr.gov.br>
2026-08-20 12:41:14 -0300

    main.js

---
+++ main.js
@@ -0,0 +1,14 @@
+const numeroSenha = document.querySelector   (.'parametro-senha__texto')
+numeroSenha. textoContent = 5;
+
+tamanhoSenha = 8
+numeroSenha
+const botoes = document.querySelector ('parametro-senha__texto')
+querySelectorAll ('parametro-senha__botao')
+
+botoes [0].onclik = diminuir;
+
+function diminuir () {
+    tamanhoSenha--;
+   numeroSenha.textoContent = tamanhoSenha
+}
